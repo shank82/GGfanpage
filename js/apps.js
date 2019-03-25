fec7de6241922ee763fd96e6f9b3622e33de8393
Shaun Hankins <gaireeh@gmail.com>
2019-03-25 04:20:15 -0400

    quiz added scoring and placing is all off

--- js/apps.js
+++ js/apps.js
@@ -3,13 +3,13 @@ function check() {
     var question1 =document.quiz.question1.value;
     var question2 =document.quiz.question2.value;
     var question3 =document.quiz.question3.value;
-    // var question4 =document.quiz.question4.value;
+    var question4 =document.quiz.question4.value;
     // var question5 =document.quiz.question5.value;
     // var question6 =document.quiz.question6.value;
     var correct= 0;
 
-      if (question1 == " The Ravagers") {
-        correct ++;
+      if (question1 == "The Ravagers") {
+        correct++;
       }
       if (question2 == "Awesome Mix Vol.1") {
         correct++;
@@ -17,8 +17,34 @@ function check() {
         if (question3 == "Yandue") {
           correct++;
       }
+      if (question4 == "Rocket") {
+        correct++;
+    }
+
+      var message = [ "You Rocked it!", "You are a Fan,but not a Super Fan", "You know nothing"];
+      var score;
+      if (correct == 4) {
+        score = 4;
+      }
+      if (correct > 2 && correct > 4) {
+        score = 3;
+      }
+      if (correct > 1 && correct < 3 ) {
+        score = 2;
+      }
+      if (correct == 1) {
+          score = 1;
+      }
+
+      if (correct == 0 ) {
+          score = 0;
+      }
+
+
 
   document.getElementById('once_checked').style.visibility = "visible";
 
-  document.getElementById("number_correct").innerHTML = "You got " + correct + "correct.";
+
+  document.getElementById("message").innerHTML = message[score];
+  document.getElementById("number_correct").innerHTML = "You got " + score + "correct.";
 }
